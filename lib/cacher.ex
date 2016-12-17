0bd4e7cc65f47fcb70618e5d7806b9df523a63e8
Landon Marder <landon.marder@gmail.com>
2016-12-17 10:51:42 -0500

    add ability to write to cache store

--- lib/cacher.ex
+++ lib/cacher.ex
@@ -1,3 +1,39 @@
 defmodule Cacher do
+  use GenServer
+  @name CA
 
+  ## Client API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts ++ [name: CA])
+  end
+
+  def write(key, value) do
+    GenServer.call(@name, {:write_to_store, key, value})
+  end
+
+
+  ## Server API
+
+  def handle_call({:write_to_store, key, value}, _from, store) do
+    new_store = update_store(store, key, value)
+    {:reply, "#{key}: #{value}", new_store}
+  end
+
+  ## Server Callbacks
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  ## Helper Functions
+
+  defp update_store(store, key, value) do
+    case Map.has_key?(store, key) do
+      true ->
+        Map.update!(store, key, value)
+      false ->
+        Map.put_new(store, key, value)
+    end
+  end
 end
